48796018e246d336cb65fd53c5945c29eb7ef111
jdboachie <judeboachiekwame@gmail.com>
2023-05-25 18:55:52 +0000

    Create lab-1.cpp

---
+++ labs/lab-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// cout << "Hello World!" << endl;
+	// return 0;
+
+	// int userAge;
+	
+	// cout << "Enter your age: "; cin >> userAge;
+	// cout << "You were born in the year " << 2023 - userAge << endl;
+
+	int marks[] = {67, 71, 54, 43, 82, 90};
+
+	marks[2] = 75;
+
+	int AppliedElectricity = marks[4];
+}
