b8184bce2f524356e55f235bb4c1009439d3dbaa
jdboachie <judeboachiekwame@gmail.com>
2023-05-27 02:12:32 +0000

    Update four-function-calculator.cpp

--- assignments/four-function-calculator.cpp
+++ assignments/four-function-calculator.cpp
@@ -1,41 +1,65 @@
-// Four-function Calculator
+// Four function calculator
 
 #include <iostream>
 
-using namespace std;
+int getUserInput(const std::string& prompt);
+char getOperator();
+void performCalculation(int num1, int num2, char op);
 
 int main()
 {
-    int num1, num2;
+    char choice;
+    do
+    {
+        int num1 = getUserInput("Enter the first number: ");
+        char op = getOperator();
+        int num2 = getUserInput("Enter the second number: ");
+
+        performCalculation(num1, num2, op);
+
+        std::cout << "Do you want to do another calculation? (Y/N): ";
+        std::cin >> choice;
+    } while (choice != 'N' && choice != 'n');
+
+    return 0;
+}
+
+int getUserInput(const std::string& prompt)
+{
+    int num;
+    std::cout << prompt;
+    std::cin >> num;
+    return num;
+}
+
+char getOperator()
+{
     char op;
+    std::cout << "Enter operator (+, -, *, /): ";
+    std::cin >> op;
+    return op;
+}
 
-    cout << "Enter first number: " << endl;
-    cin >> num1;
-
-    cout << "Enter operator: " << endl;
-    cin >> op;
-
-    cout << "Enter second number: " << endl;
-    cin >> num2;
-
+void performCalculation(int num1, int num2, char op)
+{
     switch (op)
     {
         case '+':
-            cout << num1 + num2 << endl;
+            std::cout << "Result: " << num1 + num2 << std::endl;
             break;
         case '-':
-            cout << num1 - num2 << endl;
+            std::cout << "Result: " << num1 - num2 << std::endl;
             break;
         case '*':
-            cout << num1 * num2 << endl;
+            std::cout << "Result: " << num1 * num2 << std::endl;
             break;
         case '/':
-            cout << num1 / num2 << endl;
+            if (num2 != 0)
+                std::cout << "Result: " << num1 / num2 << std::endl;
+            else
+                std::cout << "[Error] Division by zero!" << std::endl;
             break;
         default:
-            cout << "Invalid operator" << endl;
-            break;
+            std::cout << "[Error] Invalid operator!" << std::endl;
     }
-    
-    return 0; // Success
 }
