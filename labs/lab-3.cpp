2eccbf92b29d5a0106eaf41609eab489b11e8ea8
jdboachie <judeboachiekwame@gmail.com>
2023-08-11 14:20:20 +0000

    Update lab-3.cpp

--- labs/lab-3.cpp
+++ labs/lab-3.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
 int sum_to_n(int n);
 int fibonacci(int n);
 void generateFibonacciSequence(int n);
 
 int main()
 {
-    std::cout << "Lab 3" << std::endl << "1. Sum of all numbers from 1 to n" << std::endl;
+    cout << "Lab 3" << endl << "1. Sum of all numbers from 1 to n" << endl;
     int n;
-    std::cout << "Enter a number: ";
-    std::cin >> n;
-    std::cout << "The sum of all natural numbers from 1 to " << n << " is " << sum_to_n(n) << std::endl;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "The sum of all natural numbers from 1 to " << n << " is " << sum_to_n(n) << endl;
 
-    std::cout << "2. Centimeters to Feet" << std::endl;
+    cout << "2. Centimeters to Feet" << endl;
     double cm;
-    std::cout << "Enter your height in centimeters: ";
-    std::cin >> cm;
-    std::cout << "Your height in feet is " << cm / 30.48 << std::endl;
+    cout << "Enter your height in centimeters: ";
+    cin >> cm;
+    cout << "Your height in feet is " << cm / 30.48 << endl;
 
-    std::cout << "3. Fibonacci Sequence" << std::endl;
+    cout << "3. Fibonacci Sequence" << endl;
     int fib;
-    std::cout << "Enter a number: ";
-    std::cin >> fib;
+    cout << "Enter a number: ";
+    cin >> fib;
     generateFibonacciSequence(fib);
 
     return 0;
@@ -50,7 +52,7 @@ void generateFibonacciSequence(int n)
 {
     for (int i = 1; i < n; i ++)
     {
-        std::cout << fibonacci(i) << " -> ";
+        cout << fibonacci(i) << " -> ";
     }
-    std::cout << fibonacci(n) << std::endl;
+    cout << fibonacci(n) << endl;
 }
