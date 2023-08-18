5adec1617e7a5451cc228aa1b58ee48073d10bde
jdboachie <judeboachiekwame@gmail.com>
2023-08-18 21:57:23 +0000

    Update exam.cpp
    Done?
    About to add documentation

--- exam/exam.cpp
+++ exam/exam.cpp
@@ -1,7 +1,37 @@
+/**
+ * @name: Jude Boachie
+ * @index: 7095021
+ * @class: Computer Engineering 2
+ */
+
 #include <iostream>
-#include <string>
 
 using namespace std;
+
+/**
+ * @class Util
+ * @brief A utility class containing mathematical functions.
+ */
+class Util
+{
+public:
+	/**
+	 * @brief Calculate the greatest common divisor (GCD) of two numbers.
+	 *
+	 * This function calculates the GCD using the Euclidean algorithm.
+	 * It recursively calculates the GCD until one of the numbers becomes zero.
+	 *
+	 * @param a The first integer.
+	 * @param b The second integer.
+	 * @return The greatest common divisor of 'a' and 'b'.
+	 */
+	static int gcd(int a, int b)
+	{
+		if (a == 0)
+			return b;
+		return gcd(b % a, a);
+	}
+};
 
 class Fraction
 {
@@ -11,14 +41,14 @@ private:
 	int whole_number;
 
 public:
-	Fraction(int n, int d, int w=0)
+	Fraction(int n, int d, int w = 0)
 	{
 		numerator = n;
 		denominator = d;
 		whole_number = w;
 	};
 
-	float toDecimal()
+	float toDecimal() const
 	{
 		// Converting to float type
 		float w = this->whole_number;
@@ -37,14 +67,21 @@ public:
 
 	virtual void showFraction()
 	{
-		cout << numerator << "/" << denominator;
+		if (numerator == denominator)
+			cout << 1;
+		else if (numerator == 0)
+			cout << 0;
+		else
+			cout << numerator << "/" << denominator;
 	};
 
-	virtual Fraction operator+(Fraction op2){
+	virtual Fraction operator+(Fraction op2)
+	{
 		return Fraction(0, 0, 0);
 	};
 
-	virtual Fraction operator*(Fraction op2){
+	virtual Fraction operator*(Fraction op2)
+	{
 		return Fraction(0, 0, 0);
 	};
 };
@@ -54,8 +91,9 @@ class ProperFraction : public Fraction
 private:
 	int numerator;
 	int denominator;
+
 public:
-	ProperFraction (int n, int d) : Fraction (n, d, 0)
+	ProperFraction(int n, int d) : Fraction(n, d, 0)
 	{
 		this->numerator = n;
 		this->denominator = d;
@@ -69,11 +107,27 @@ public:
 
 		int newNumerator = (n1 * d2) + (d1 * n2);
 		int newDenominator = d1 * d2;
-		return ProperFraction(newNumerator, newDenominator);
+
+		// reduce the fraction
+		int GCD = Util::gcd(newNumerator, newDenominator);
+
+		return ProperFraction(newNumerator / GCD, newDenominator / GCD);
 	};
+
 	ProperFraction operator*(ProperFraction op2)
 	{
-		return ProperFraction(0, 0);
+		int n1 = this->numerator;
+		int d1 = this->denominator;
+		int n2 = op2.numerator;
+		int d2 = op2.denominator;
+
+		int newNumerator = n1 * n2;
+		int newDenominator = d1 * d2;
+
+		// reduce the fraction
+		int GCD = Util::gcd(newNumerator, newDenominator);
+
+		return ProperFraction(newNumerator / GCD, newDenominator / GCD);
 	};
 };
 
@@ -86,39 +140,121 @@ private:
 	bool mixed;
 
 public:
-	ImproperFraction(int n, int d, int w=0, bool m=false) : Fraction(n, d, w)
+	ImproperFraction(int n, int d, int w = 0) : Fraction(n, d, w)
 	{
 		this->numerator = n;
 		this->denominator = d;
 		this->whole_number = w;
-		this->mixed = m;
+
+		if (w == 0)
+			this->mixed = false;
+		else
+			this->mixed = true;
 	};
 
-	void toMixedFraction(){};
-	void toImproperFraction(){};
+	void toMixedFraction()
+	{
+		if (!this->mixed)
+		{
+			this->whole_number = this->numerator / this->denominator;
+			this->numerator = this->numerator % this->denominator;
+			this->mixed = true;
+		}
+	};
+
+	void toImproperFraction()
+	{
+		if (this->mixed)
+		{
+			this->numerator = (this->whole_number * this->denominator) + this->numerator;
+			this->whole_number = 0;
+			this->mixed = false;
+		}
+	};
 
 	void showFraction() override
 	{
-		cout << this->whole_number << "(" << this->numerator << "/" << this->denominator << ")";
+		if (numerator == 0)
+			cout << this->whole_number;
+		else if (this->mixed)
+			cout << this->whole_number << "'" << this->numerator << "/" << this->denominator;
+		else
+			cout << this->numerator << "/" << this->denominator;
+	};
+
+	ImproperFraction operator+(ImproperFraction op2)
+	{
+		this->toImproperFraction();
+		op2.toImproperFraction();
+
+		int n1 = this->numerator;
+		int d1 = this->denominator;
+		int n2 = op2.numerator;
+		int d2 = op2.denominator;
+
+		int newNumerator = (n1 * d2) + (d1 * n2);
+		int newDenominator = d1 * d2;
+
+		ImproperFraction result(newNumerator, newDenominator);
+		result.toMixedFraction();
+
+		return result;
+	};
+
+	ImproperFraction operator*(ImproperFraction op2)
+	{
+		this->toImproperFraction();
+		op2.toImproperFraction();
+
+		int n1 = this->numerator;
+		int d1 = this->denominator;
+		int n2 = op2.numerator;
+		int d2 = op2.denominator;
+
+		int newNumerator = n1 * n2;
+		int newDenominator = d1 * d2;
+
+		ImproperFraction result(newNumerator, newDenominator);
+		result.toMixedFraction();
+		return result;
 	};
 };
 
 int main()
 {
 	ProperFraction f1(1, 2);
-	ProperFraction f2(1, 3);
-	ImproperFraction f3(1, 2, 3);
+	ProperFraction f2(5, 3); // Counterintuitive but it's a proper fraction
+	ImproperFraction f3(3, 4, 2);
+	ImproperFraction f4(5, 3);
 
-	ProperFraction f4 = f1 + f1;
+	// Solution Fractions
+	ProperFraction ansA = f1 + f1;
+	ImproperFraction ansB = f3 + f4;
+	ProperFraction ansC = f2 * f1;
+	ImproperFraction ansD = f3 * f4;
 
+	cout << "a. ";
+	ansA.showFraction();
+	cout << endl
+		 << "b. ";
+	ansB.showFraction();
+	cout << endl
+		 << "c. ";
+	ansC.showFraction();
+	cout << endl
+		 << "d. ";
+	ansD.showFraction();
+	cout << endl
+		 << "e. Abi your eye get XD?" << endl
+		 << "f. "
+		 << f1.toDecimal() << endl
+		 << "g. "
+		 << f2.toDecimal() << endl
+		 << "h. "
+		 << f3.toDecimal() << endl
+		 << "i. On point!" << endl;
 
-	f1.showFraction();
-	cout << " + ";
-	f1.showFraction();
-	cout << " = ";
-	f4.showFraction();
-	cout << " (" << f4.toDecimal() << ")" << endl;
-
+	// 0 for success
 	return 0;
 }
 
